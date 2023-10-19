8195cdc6ee7b3978c4662d52fcc39763233a75f7
AgustinAzul <agustds@gmail.com>
2023-10-19 12:17:47 -0300

    Se agrega el proyecto

---
+++ src/main/java/com/mycompany/pruebaconmaven/modelo/prueba1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pruebaconmaven.modelo;
+
+/**
+ *
+ * @author agust
+ */
+public class prueba1 {
+    private int edad;
+    private int peso;
+    private int cantMascotas;
+}
