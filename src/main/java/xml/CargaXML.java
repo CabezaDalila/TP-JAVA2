59959aa919c73a5278d3e7f5a3ee7f3dc90b0a9d
AgustinAzul <agustds@gmail.com>
2023-10-20 15:34:28 -0300

    creando arhivo xml

---
+++ src/main/java/xml/CargaXML.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package xml;
+
+/**
+ *
+ * @author agust
+ */
+public class CargaXML {
+    
+}
